e5dcd1de5fcfcdb93a4b1ed20512bf1777f2c66a
Brian Marriott <brian@marriott.id.au>
2023-02-17 09:47:55 +1100

    Added an additional command to the "bash-commands" file to update the search index

--- bash-commands-for-publishing.sh
+++ bash-commands-for-publishing.sh
@@ -3,3 +3,7 @@ bundle install
 
 #to host locally:
 bundle exec jekyll serve
+
+#to update the search index (using your local content)
+ALGOLIA_API_KEY=secret-key bundle exec jekyll algolia
+# "secret-key" is in the password safe (under algolia)
